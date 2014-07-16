b0d448f6c04ec141396d52c848019f500f84cc01
adduri <sagarkache1@gmail.com>
2014-07-16 13:23:21 +0530

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :name,:email,:firstname,:lastname
 
 	# validates :name,:lastname,:firstname, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-	# validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates_presence_of :name
 end
